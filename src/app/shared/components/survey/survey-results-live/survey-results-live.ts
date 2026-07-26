6ff58bb1beece772c194cf1b2028a4f687a02225
batti251 <bastibuenz25@gmail.com>
2026-07-26 08:06:29 +0200

    fix (survey-view): uncomment navigtion-function for proper forwarding, after submit, adjust dialog-styling

--- src/app/shared/components/survey/survey-results-live/survey-results-live.ts
+++ src/app/shared/components/survey/survey-results-live/survey-results-live.ts
@@ -19,7 +19,6 @@ export class SurveyResultsLive {
   async ngOnInit() {
     let surveyId = this.activatedRoute.snapshot.paramMap.get('id') as string
     await this.db.loadLiveSurvey('surveys', surveyId)
-    
   }
 
   ngOnDestroy(){
